6818067cce729a86ccbd45db09b7203e4f7731ca
chtsrl <chtsrl@gmail.com>
2023-01-05 23:14:28 +0300

    “cv6”

--- lib/models/song_model.dart
+++ lib/models/song_model.dart
@@ -13,60 +13,64 @@ class Song {
 
   static List<Song> songs = [
     Song(
-      title: 'Zorundamıyım',
+      title: 'Zorunda miyim',
       description: 'Dilberay & Şahan',
-      url:
-          'assets/music/Zorundamıyım  Şahan  Dilber ay  [ Full ]Turkcell Reklamları.mp3',
-      coverUrl: 'assets/images/zorundamıyım_background.jpg',
+      url: 'assets/music/zorundamiyim.mp3',
+      coverUrl: 'assets/images/zorundamiyim.jpeg',
     ),
     Song(
-      title: 'AC&DC',
-      description: 'Rock',
-      url: 'assets/music/AC_DC - Highway to Hell.mp3',
-      coverUrl: 'assets/images/adcd_background.jpeg',
+      title: 'Best Of',
+      description: 'GOOGGOOSH',
+      url: 'assets/music/googoosh.mp3',
+      coverUrl: 'assets/images/googoosh.jpeg',
+    ),
+    Song(
+      title: 'Fenerbahce',
+      description: 'En Buyuk Cimbom',
+      url: 'assets/music/cimbom.mp3',
+      coverUrl: 'assets/images/shitteam.jpeg',
+    ),
+    Song(
+      title: 'Ultra Aslan',
+      description: 'GALATASARAY',
+      url: 'assets/music/kirac.mp3',
+      coverUrl: 'assets/images/cimbom.jpg',
     ),
     Song(
       title: 'Metallica',
       description: 'Rock',
-      url: 'assets/music/Metallica - Enter Sandman.mp3',
+      url: 'assets/music/zorundamiyim.mp3',
       coverUrl: 'assets/images/metallica_background.jpeg',
     ),
     Song(
       title: 'Martin Garrix',
       description: 'EDM',
-      url:
-          'assets/music/Martin Garrix - Hold On & Believe (feat. The Federal Empire).mp3',
+      url: 'assets/music/zorundamiyim.mp3',
       coverUrl: 'assets/images/martingarrix_background.jpeg',
     ),
     Song(
       title: 'The Weeknd',
       description: 'Big',
-      url: 'assets/music/The Weeknd - Blinding Lights (Official Audio).mp3',
+      url: 'assets/music/zorundamiyim.mp3',
       coverUrl: 'assets/images/theweeknd_background.jpeg',
     ),
     Song(
       title: 'No1',
       description: 'Turkish Rap',
-      url: 'assets/music/mp3indirdur-No-1-Boyle-Iyi.mp3',
+      url: 'assets/music/zorundamiyim.mp3',
       coverUrl: 'assets/images/No1_background.jpeg',
     ),
     Song(
       title: 'Drake',
       description: 'Epic',
-      url: 'assets/music/Drake, 21 Savage - Rich Flex (Audio).mp3',
+      url: 'assets/music/zorundamiyim.mp3',
       coverUrl: 'assets/images/drake_background.jpeg',
     ),
     Song(
       title: 'Hadise',
       description: 'Turkish Pop',
-      url: 'assets/music/Hadise - Küçük Bir Yol.mp3',
+      url: 'assets/music/zorundamiyim.mp3',
       coverUrl: 'assets/images/hadise_background.jpeg',
     ),
-    Song(
-      title: 'Formula 1',
-      description: 'Racing',
-      url: 'assets/music/formula-1-theme-song.mp3',
-      coverUrl: 'assets/images/f1_background.jpeg',
-    )
   ];
 }
